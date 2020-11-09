db7b642d0f40065cabb7f607134f441db7ced73e
Jurgen Leschner <jldec@ciaosoft.com>
2020-11-09 11:46:30 -0500

    v1.2.8 more visible permalink

--- plugins/generator-plugin.js
+++ plugins/generator-plugin.js
@@ -54,8 +54,9 @@ module.exports = function(generator) {
 
   hb.registerHelper('permaLink', function(frame) {
     return '<a class="permalink" href="' +
-      hb.relPath() + this._href +
-      '" title="Link to this post.">&#xf0c1;</a>'
+      hb.relPath() + this._href + '" title="Link to this post.">' +
+      opts.appUrl + this._href
+      +'</a>'
   });
 
   // block helper over posts
